5a8ae1f255915e4e292c8aaf0da5e2957a66620b
Bart Scheyltjens <bartscheyltjens@hotmail.com>
2021-02-22 18:53:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write($"Voornaam : ");
+			string voornaam = Console.ReadLine();
+			Console.Write($"naam : ");
+			string naam = Console.ReadLine();
+			Console.Write($"straat : ");
+			string straat = Console.ReadLine(); 
+			Console.Write($"huisnummer :");
+			string huisnummer = Console.ReadLine();
+			Console.Write($"woonplaats :");
+			string woonplaats = Console.ReadLine();
+			Console.Write($"postcode :");
+			string postcode = Console.ReadLine();
+
+
+			Console.Write($"Naam :");
+			string Naam = Console.ReadLine();
+			Console.Write($"adress :");
+			string adress = Console.ReadLine();
+
+
+
+
+
+
+		}
+	}
+}
